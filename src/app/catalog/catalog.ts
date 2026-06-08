0527ae47cb640e42cb8d73a0e5a2c6d7b60d4a93
Lester Antonio Rodríguez Tenorio <lestertenorio30@gmail.com>
2026-06-08 10:35:52 -0600

    v1.1.3

--- src/app/catalog/catalog.ts
+++ src/app/catalog/catalog.ts
@@ -39,56 +39,150 @@ export class Catalog implements AfterViewInit {
         reveals.forEach(el => observer.observe(el));
     }
 
+    // products = [
+    //     {
+    //         name: "Pasos Clásicas",
+    //         description: "Diseño ligero y cómodo para uso diario, ideal tanto en casa como para salidas rápidas.",
+    //         price: 150,
+    //         image: "Chanclas Clásicas Pasos.png",
+    //         tags: ["Comodidad", "Uso diario"]
+    //     },
+    //     {
+    //         name: "Pasos Urbanas",
+    //         description: "Modelo cerrado y estable que brinda mayor protección y soporte en cada paso.",
+    //         price: 180,
+    //         image: "Slides Cerradas Pasos.png",
+    //         tags: ["Protección", "Ergonómica"]
+    //     },
+    //     {
+    //         name: "Pasos Air",
+    //         description: "Diseño fresco y versátil, pensado para climas cálidos y uso prolongado sin incomodidad.",
+    //         price: 160,
+    //         image: "Slides Abiertas Pasos.png",
+    //         tags: ["Comodidad", "Frescura"]
+    //     },
+    //     {
+    //         name: "Pasos Urban Flow",
+    //         description: "Modelo ligero con ajuste flexible que acompaña el movimiento natural del pie durante todo el día.",
+    //         price: 200,
+    //         image: "Chinelas Pasos/1.jpeg",
+    //         tags: ["Urbano", "Uso diario"]
+    //     },
+    //     {
+    //         name: "Pasos Nova",
+    //         description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+    //         price: 220,
+    //         image: "Chinelas Pasos/2.jpeg",
+    //         tags: ["Ligereza", "Urbano"]
+    //     },
+    //     {
+    //         name: "Pasos Breeze",
+    //         description: "Diseñadas para máxima ventilación, ofreciendo frescura constante incluso en los días más calurosos.",
+    //         price: 220,
+    //         image: "Chinelas Pasos/3.jpeg",
+    //         tags: ["Frescura", "Verano"]
+    //     },
+    //     {
+    //         name: "Pasos Titan",
+    //         description: "Modelo reforzado con mayor durabilidad y soporte, pensado para uso intensivo y terrenos exigentes.",
+    //         price: 250,
+    //         image: "Chinelas Pasos/4.jpeg",
+    //         tags: ["Resistente", "Exclusivo"]
+    //     }
+    // ];
+
     products = [
         {
-            name: "Pasos Clásicas",
-            description: "Diseño ligero y cómodo para uso diario, ideal tanto en casa como para salidas rápidas.",
-            price: 150,
-            image: "Chanclas Clásicas Pasos.png",
-            tags: ["Comodidad", "Uso diario"]
-        },
-        {
-            name: "Pasos Urbanas",
-            description: "Modelo cerrado y estable que brinda mayor protección y soporte en cada paso.",
-            price: 180,
-            image: "Slides Cerradas Pasos.png",
-            tags: ["Protección", "Ergonómica"]
-        },
-        {
-            name: "Pasos Air",
-            description: "Diseño fresco y versátil, pensado para climas cálidos y uso prolongado sin incomodidad.",
-            price: 160,
-            image: "Slides Abiertas Pasos.png",
-            tags: ["Comodidad", "Frescura"]
-        },
-        {
-            name: "Pasos Urban Flow",
-            description: "Modelo ligero con ajuste flexible que acompaña el movimiento natural del pie durante todo el día.",
-            price: 200,
-            image: "Chinelas Pasos/1.jpeg",
-            tags: ["Urbano", "Uso diario"]
-        },
-        {
-            name: "Pasos Nova",
+            name: "Pasos",
             description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
             price: 220,
-            image: "Chinelas Pasos/2.jpeg",
-            tags: ["Ligereza", "Urbano"]
+            image: "Catalogo/5.png",
+            tags: ["Comodidad", "Originalidad"]
         },
         {
-            name: "Pasos Breeze",
-            description: "Diseñadas para máxima ventilación, ofreciendo frescura constante incluso en los días más calurosos.",
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
             price: 220,
-            image: "Chinelas Pasos/3.jpeg",
-            tags: ["Frescura", "Verano"]
+            image: "Catalogo/8.png",
+            tags: ["Comodidad", "Originalidad"]
         },
         {
-            name: "Pasos Titan",
-            description: "Modelo reforzado con mayor durabilidad y soporte, pensado para uso intensivo y terrenos exigentes.",
-            price: 250,
-            image: "Chinelas Pasos/4.jpeg",
-            tags: ["Resistente", "Exclusivo"]
-        }
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/9.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 1.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 2.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 3.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 4.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 5.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 6.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 7.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 8.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 9.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
+        {
+            name: "Pasos",
+            description: "Estilo minimalista y suave al caminar, ideal para uso diario dentro y fuera de casa.",
+            price: 220,
+            image: "Catalogo/Pasos 10.png",
+            tags: ["Comodidad", "Originalidad"]
+        },
     ];
 
     readonly state = signal<'closed' | 'open' | 'closing'>('closed');
